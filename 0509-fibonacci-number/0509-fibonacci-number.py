6febd783220e692520b25a113f0a0e53013214c0
amanueldemirew <121667469+amanueldemirew@users.noreply.github.com>
2023-02-03 01:37:44 +0300

    Time: 815 ms (16.69%), Space: 13.8 MB (46.98%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,11 +1,6 @@
 class Solution:
     def fib(self, n: int) -> int:
-        a,b = 0,1
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-        else:
-            for i in range(2,n+1):
-                a,b = b,a+b
-            return b
+        
+        if n <=1:
+            return n
+        return Solution.fib(self,n-1) + Solution.fib(self,n-2)
